b4b66d8850d1cb9902b637fd6279b44eb4a0209c
Aldo <38758944+AldoFusterTurpin@users.noreply.github.com>
2019-10-29 12:41:09 +0100

    Update tour_of_go_exercise_maps.go

--- tour_of_go_exercise_maps.go
+++ tour_of_go_exercise_maps.go
@@ -4,43 +4,39 @@ package main
 
 import (
 	"strings"
-
 	"golang.org/x/tour/wc"
 )
 
 func WordCount_Go_is_great(s string) map[string]int {
 	m := make(map[string]int)
-
 	for _, c := range strings.Fields(s) {
-		m[c] += 1
+	    m[c] += 1
 	}
 	return m
 }
 
 func WordCount(s string) map[string]int {
 	m := make(map[string]int)
-
 	for _, c := range strings.Fields(s) {
-		_, ok := m[c]
-		if ok {
-			m[c] += 1
-		} else {
-			m[c] = 1
-		}
+	    _, ok := m[c]
+	    if ok {
+                m[c] += 1
+	    } else {
+                m[c] = 1
+	    }
 	}
 	return m
 }
 
 func WordCount_v2(s string) map[string]int {
 	m := make(map[string]int)
-
 	for _, c := range strings.Fields(s) {
-		elem, ok := m[c]
-		if ok {
-			m[c] = elem + 1
-		} else {
-			m[c] = 1
-		}
+	    elem, ok := m[c]
+	    if ok {
+	        m[c] = elem + 1
+	    } else {
+                m[c] = 1
+	    }
 	}
 	return m
 }
